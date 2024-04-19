9183aa112f168afb3c83206879f0bbe030d2bfe2
Deen Ahmed <ranadeenahmed@gmail.com>
2024-04-19 15:01:37 +0100

    feat: add answers into btn

--- assets/quiz.js
+++ assets/quiz.js
@@ -45,7 +45,33 @@ const questions = [
     ],
   },
 ];
+// questions
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+// const to pull Id from html
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
+// function to pull question to correct place
