40f80970a87b2e2ef5bff82adaa7f7cf1a4dfbca
Deen Ahmed <ranadeenahmed@gmail.com>
2024-04-19 14:38:51 +0100

    feat: added questions to js

---
+++ assets/quiz.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    question: "Who has scored the most points in NBA history?",
+    answers: [
+      { text: "A. Michael Jordan", correct: false },
+      { text: "B. Lebron James", correct: true },
+      { text: "C. Kareem Abdul-Jabbar", correct: false },
+      { text: "D. Wilt Chamberlain", correct: false },
+    ],
+  },
+  {
+    question: "Which NBA Team has the most Championship titles?",
+    answers: [
+      { text: "A. Chicago Bulls", correct: false },
+      { text: "B. Los Angeles Lakers", correct: true },
+      { text: "C. Golden State Warriors", correct: false },
+      { text: "D. San Antiono Spurs", correct: false },
+    ],
+  },
+  {
+    question: "Who has won the most NBA Championship titles?",
+    answers: [
+      { text: "A. Michael Jordan", correct: false },
+      { text: "B. Larry Bird", correct: false },
+      { text: "C. Tim Duncan", correct: false },
+      { text: "D. Robert Horry", correct: true },
+    ],
+  },
+  {
+    question: "Which player has the most MVPs?",
+    answers: [
+      { text: "A. Lebron James", correct: false },
+      { text: "B. Kareem Abdul-Jabbar", correct: false },
+      { text: "C. Bill Russell", correct: true },
+      { text: "D. Michael Jordan", correct: false },
+    ],
+  },
+  {
+    question: "Which of these NBA franchises has never relocated?",
+    answers: [
+      { text: "A. Toronto Raptors", correct: true },
+      { text: "B. Utah Jazz", correct: false },
+      { text: "C. Sacramento Kings", correct: false },
+      { text: "D. Los Angeles Lakers", correct: false },
+    ],
+  },
+];
