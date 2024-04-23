321be6b7bfaf30b14c599658773eed0e0568114e
Deen Ahmed <ranadeenahmed@gmail.com>
2024-04-23 21:50:20 +0100

    feat: more questions added

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -2,46 +2,186 @@ const questions = [
   {
     question: "Who has scored the most points in NBA history?",
     answers: [
-      { text: "A. Michael Jordan", correct: false },
-      { text: "B. Lebron James", correct: true },
-      { text: "C. Kareem Abdul-Jabbar", correct: false },
-      { text: "D. Wilt Chamberlain", correct: false },
+      { text: "Michael Jordan", correct: false },
+      { text: "Lebron James", correct: true },
+      { text: "Kareem Abdul-Jabbar", correct: false },
+      { text: "Wilt Chamberlain", correct: false },
     ],
   },
   {
     question: "Which NBA Team has the most Championship titles?",
     answers: [
-      { text: "A. Chicago Bulls", correct: false },
-      { text: "B. Los Angeles Lakers", correct: true },
-      { text: "C. Golden State Warriors", correct: false },
-      { text: "D. San Antiono Spurs", correct: false },
+      { text: "Chicago Bulls", correct: false },
+      { text: "Los Angeles Lakers", correct: true },
+      { text: "Golden State Warriors", correct: false },
+      { text: "San Antiono Spurs", correct: false },
     ],
   },
   {
     question: "Who has won the most NBA Championship titles?",
     answers: [
-      { text: "A. Michael Jordan", correct: false },
-      { text: "B. Larry Bird", correct: false },
-      { text: "C. Tim Duncan", correct: false },
-      { text: "D. Robert Horry", correct: true },
+      { text: "Michael Jordan", correct: false },
+      { text: "Larry Bird", correct: false },
+      { text: "Tim Duncan", correct: false },
+      { text: "Robert Horry", correct: true },
     ],
   },
   {
     question: "Which player has the most MVPs?",
     answers: [
-      { text: "A. Lebron James", correct: false },
-      { text: "B. Kareem Abdul-Jabbar", correct: true },
-      { text: "C. Bill Russell", correct: false },
-      { text: "D. Michael Jordan", correct: false },
+      { text: "Lebron James", correct: false },
+      { text: "Kareem Abdul-Jabbar", correct: true },
+      { text: "Bill Russell", correct: false },
+      { text: "Michael Jordan", correct: false },
     ],
   },
   {
     question: "Which of these NBA franchises has never relocated?",
     answers: [
-      { text: "A. Toronto Raptors", correct: true },
-      { text: "B. Utah Jazz", correct: false },
-      { text: "C. Sacramento Kings", correct: false },
-      { text: "D. Los Angeles Lakers", correct: false },
+      { text: "Toronto Raptors", correct: true },
+      { text: "Utah Jazz", correct: false },
+      { text: "Sacramento Kings", correct: false },
+      { text: "Los Angeles Lakers", correct: false },
+    ],
+  },
+  {
+    question: "Who has scored the most points in a single NBA game?",
+    answers: [
+      { text: "Kobe Bryant", correct: false },
+      { text: "Wilt Chamberlain", correct: true },
+      { text: "Michael Jordan", correct: false },
+      { text: "LeBron James", correct: false },
+    ],
+  },
+  {
+    question: "Which NBA player holds the record for the most career assists?",
+    answers: [
+      { text: "Magic Johnson", correct: false },
+      { text: "John Stockton", correct: true },
+      { text: "Steve Nash", correct: false },
+      { text: "Chris Paul", correct: false },
+    ],
+  },
+  {
+    question: "Who is known as 'The Big Fundamental' in basketball?",
+    answers: [
+      { text: "Shaquille O'Neal", correct: false },
+      { text: "Tim Duncan", correct: true },
+      { text: "Karl Malone", correct: false },
+      { text: "Hakeem Olajuwon", correct: false },
+    ],
+  },
+  {
+    question: "Which player has won the most NBA championships?",
+    answers: [
+      { text: "A. Michael Jordan", correct: false },
+      { text: "B. Bill Russell", correct: true },
+      { text: "C. Magic Johnson", correct: false },
+      { text: "D. Kareem Abdul-Jabbar", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who was the first player to be drafted straight out of high school to the NBA?",
+    answers: [
+      { text: "Kevin Garnett", correct: true },
+      { text: "Kobe Bryant", correct: false },
+      { text: "LeBron James", correct: false },
+      { text: "Dwight Howard", correct: false },
+    ],
+  },
+  {
+    question: "Which NBA team has won the most championships?",
+    answers: [
+      { text: "Los Angeles Lakers", correct: false },
+      { text: "Boston Celtics", correct: true },
+      { text: "Chicago Bulls", correct: false },
+      { text: "Golden State Warriors", correct: false },
+    ],
+  },
+  {
+    question: "Who is the only player to win NBA MVP with a losing record?",
+    answers: [
+      { text: "LeBron James", correct: false },
+      { text: "Russell Westbrook", correct: true },
+      { text: "Kobe Bryant", correct: false },
+      { text: "Stephen Curry", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which player holds the record for the highest career points per game average in the NBA?",
+    answers: [
+      { text: "Michael Jordan", correct: false },
+      { text: "Wilt Chamberlain", correct: true },
+      { text: "LeBron James", correct: false },
+      { text: "Kareem Abdul-Jabbar", correct: false },
+    ],
+  },
+  {
+    question: "Who is the NBA's all-time leader in three-pointers made?",
+    answers: [
+      { text: "Ray Allen", correct: false },
+      { text: "Reggie Miller", correct: false },
+      { text: "Stephen Curry", correct: true },
+      { text: "Klay Thompson", correct: false },
+    ],
+  },
+  {
+    question: "Who was the shortest player ever to play in the NBA?",
+    answers: [
+      { text: "Spud Webb", correct: false },
+      { text: "Muggsy Bogues", correct: true },
+      { text: "Nate Robinson", correct: false },
+      { text: "Earl Boykins", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who holds the record for the most rebounds in a single NBA game?",
+    answers: [
+      { text: "Bill Russell", correct: false },
+      { text: "Wilt Chamberlain", correct: true },
+      { text: "Moses Malone", correct: false },
+      { text: "Dennis Rodman", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who was the first foreign-born player to be drafted first overall in the NBA?",
+    answers: [
+      { text: "Yao Ming", correct: false },
+      { text: "Andrea Bargnani", correct: true },
+      { text: "Dirk Nowitzki", correct: false },
+      { text: "Pau Gasol", correct: false },
+    ],
+  },
+  {
+    question: "Which NBA team won the first-ever NBA Championship?",
+    answers: [
+      { text: "Boston Celtics", correct: true },
+      { text: "Minneapolis Lakers", correct: false },
+      { text: "Chicago Stags", correct: false },
+      { text: "New York Knicks", correct: false },
+    ],
+  },
+  {
+    question: "Who is the youngest player to score 10,000 points in the NBA?",
+    answers: [
+      { text: "LeBron James", correct: false },
+      { text: "Kevin Durant", correct: true },
+      { text: "Kobe Bryant", correct: false },
+      { text: "Carmelo Anthony", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who holds the record for the most blocks in a single NBA season?",
+    answers: [
+      { text: "Hakeem Olajuwon", correct: false },
+      { text: "Mark Eaton", correct: true },
+      { text: "Dikembe Mutombo", correct: false },
+      { text: "Shaquille O'Neal", correct: false },
     ],
   },
 ];
